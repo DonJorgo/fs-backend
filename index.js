f60e24b8389cd2a1ecec8c6851f2d5774be03ecb
Jorma Nieminen <donjorgo@gmail.com>
2021-07-01 19:09:53 +0100

    3.17*: puhelinluettelo ja tietokanta, step5

--- index.js
+++ index.js
@@ -20,33 +20,11 @@ morgan.token('body', (req, res) =>
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-    },
-    {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-    },
-    {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-    },
-    {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-    }
-]
-
 const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
+
 
 app.get('/info', (request, response) => {
     Person.count().then(count => {
@@ -57,25 +35,27 @@ app.get('/info', (request, response) => {
     })
 })
 
+
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => {
         response.json(persons)
     })
-
 })
+
 
 app.get('/api/persons/:id', (request, response, next) => {
-    Person.findById(request.params.id).then(person => {
-        response.json(person)
-    }).catch(error => next(error))
+    Person.findById(request.params.id)
+        .then(person => {
+            response.json(person)
+        })
+        .catch(error => next(error))
 })
+
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
-    const MAX_ID = 5000000
 
     const validation = validate(request.body)
-
     if (validation.errors) {
         return response.status(404).json({
             errors: validation.errors
@@ -86,36 +66,49 @@ app.post('/api/persons', (request, response) => {
         name: body.name,
         number: body.number,
     })
-
     person.save()
         .then(savedPerson => response.json(savedPerson))
         .catch(error => next(error))
 })
 
 const validate = person => {
-    const nameExists = name =>
-        persons.some(person => person.name === name)
-
     let errors = []
     if (!person.name) {
         errors.push("missing name")
-    } else if (nameExists(person.name)) {
-        errors.push("name must be unique")
     }
     if (!person.number) {
         errors.push("missing number")
     }
-
-    return errors.length > 0
-        ? { errors: errors }
-        : {}
+    return errors.length > 0 ? { errors: errors } : {}
 }
+
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            if (updatedPerson) {
+                response.json(updatedPerson)
+            } else {
+                response.status(404).end()
+            }
+
+        })
+        .catch(error => next(error))
+})
+
 
 app.delete('/api/persons/:id', (req, res, next) => {
     Person.findByIdAndRemove(req.params.id)
         .then(result => res.status(204).end())
         .catch(error => next(error))
 })
+
 
 const errorHandler = (error, request, response, next) => {
     console.log(error)
